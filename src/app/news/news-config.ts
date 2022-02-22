a3eb35b9645fe5454a79f8b79d09f16dd04d4d55
Mo Afolayan <mo.afolayan@gmail.com>
2022-02-22 09:04:47 -0600

    Update news-config.ts

--- src/app/news/news-config.ts
+++ src/app/news/news-config.ts
@@ -4,19 +4,19 @@ export const newsConfig = {
         url: 'https://newsapi.org/v2/top-headlines?country=us&sortBy=publishedAt&apiKey=047211f713cf4eb09e845c9739c86aa9',
         observable: null,
     },
-    // businessNews: {
-    //     displayName: 'Business',
-    //     url: 'https://newsapi.org/v2/top-headlines?country=us&category=business&sortBy=publishedAt&apiKey=047211f713cf4eb09e845c9739c86aa9',
-    //     observable: null,
-    // },
-    // wallStreetJournalNews: {
-    //     displayName: 'Wall Street Journal',
-    //     url: 'https://newsapi.org/v2/everything?domains=wsj.com&sortBy=publishedAt&apiKey=047211f713cf4eb09e845c9739c86aa9',
-    //     observable: null,
-    // },
-    // techNews: {
-    //     displayName: 'Tech',
-    //     url: 'https://newsapi.org/v2/top-headlines?sources=techcrunch&sortBy=publishedAt&apiKey=047211f713cf4eb09e845c9739c86aa9',
-    //     observable: null,
-    // },
+    businessNews: {
+        displayName: 'Business',
+        url: 'https://newsapi.org/v2/top-headlines?country=us&category=business&sortBy=publishedAt&apiKey=047211f713cf4eb09e845c9739c86aa9',
+        observable: null,
+    },
+    wallStreetJournalNews: {
+        displayName: 'Wall Street Journal',
+        url: 'https://newsapi.org/v2/everything?domains=wsj.com&sortBy=publishedAt&apiKey=047211f713cf4eb09e845c9739c86aa9',
+        observable: null,
+    },
+    techNews: {
+        displayName: 'Tech',
+        url: 'https://newsapi.org/v2/top-headlines?sources=techcrunch&sortBy=publishedAt&apiKey=047211f713cf4eb09e845c9739c86aa9',
+        observable: null,
+    },
 };
